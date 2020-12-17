fcf13573c23202db6d9312dc44e200232ac5492e
kohone <1914140+kohone@users.noreply.github.com>
2020-12-17 10:12:24 +0200

    Update Controller.php

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -14,6 +14,7 @@ class Controller extends BaseController
      */
     public function __construct()
     {
-        $this->middleware(Authenticate::class);
+        // опираемся только на config('middleware')
+        //$this->middleware(Authenticate::class);
     }
 }
